877cdce62406c6a7a918a873757f25d448c2f62a
Slamjack <murlouk@gmail.com>
2015-12-16 19:38:19 +0100

    Implementation of multiplayer
    Multiplayer base

---
+++ Assets/2_Scripts/MatchCreator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using UnityEngine.Networking.Match;
+using System.Collections.Generic;
+
+public class MatchCreator : MonoBehaviour
+{
+    List<MatchDesc> MatchList = new List<MatchDesc>();
+    NetworkMatch networkMatch;
+
+    void Awake()
+    {
+        networkMatch = gameObject.AddComponent<NetworkMatch>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void CreateMatch()
+    {
+        CreateMatchRequest MatchRequest = new CreateMatchRequest();
+        MatchRequest.name = "Match " + System.Guid.NewGuid().ToString("N");
+        MatchRequest.size = 10;
+        MatchRequest.advertise = true;
+        MatchRequest.password = "";
+
+        MatchRequest.matchAttributes = new Dictionary<string, long>();
+        MatchRequest.matchAttributes.Add(MatchProperty.MapName, 1);
+        MatchRequest.matchAttributes.Add(MatchProperty.HostName, 1);
+
+        networkMatch.CreateMatch(MatchRequest, OnMatchCreate);
+    }
+
+    public void OnMatchCreate(CreateMatchResponse matchResponse)
+    {
+        if (matchResponse.success)
+            Debug.Log("Create match succeeded");
+        else
+            Debug.LogError("Create match failed");
+    }
+}
---
+++ Assets/2_Scripts/MatchProperty.cs
@@ -0,0 +1,6 @@
+﻿public class MatchProperty
+{
+    public const string DisplayName = "DN";
+    public const string MapName = "MN";
+    public const string HostName = "HN";
+}
