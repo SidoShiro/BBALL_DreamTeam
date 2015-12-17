9fd0a5a390566941d3a7f416a5717247d16ee59d
Slamjack <murlouk@gmail.com>
2015-12-17 16:59:18 +0100

    Added MatchProperty.cs comments
    Self explanatory

--- Assets/2_Scripts/MatchProperty.cs
+++ Assets/2_Scripts/MatchProperty.cs
@@ -1,6 +1,9 @@
-﻿public class MatchProperty
+﻿/// <summary>
+/// This class is used in match creation for custom attributes. This mainly clarifies the process and prevents typography errors.
+/// </summary>
+public class MatchProperty
 {
-    public const string DisplayName = "DN";
-    public const string MapName = "MN";
-    public const string HostName = "HN";
+    public const string DisplayName = "DN"; //Name to display to the network        (e.g. "DreamTeam's server")
+    public const string MapName = "MN";     //Name of original creator of the match (e.g. "DarkSasuke92")
+    public const string HostName = "HN";    //Map to join when joining the match    (e.g. "sand_box")
 }
