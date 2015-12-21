530d2279a1dcc4bd030caa0a9e5453f6d331a8e3
PlusMaxence <maxence.caronlasne@gmail.com>
2015-12-21 16:22:06 +0100

    Add prototype of Score Module
    Premier jet du module de score.
    
    Ajout de 3 prefabs :
    Player : un prototype de joueur, avec un attribut booléen indiquant si
    il a la balle ou pas.
    Panier : un collider qui détecte si un joueur avec la balle lui rentre
    dedans.
    ScoreGM : un empty qui collecte les infos des paniers, et compte le
    score.

---
+++ Assets/2_Scripts/Score/Panier.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Panier : MonoBehaviour {
+
+    public int team; // Team du panier
+    public bool valid_point; // True si un point est marqué.
+
+	// Use this for initialization
+	void Start () {
+        valid_point = false;
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Player player = collider.gameObject.GetComponent<Player>();
+        if (player.have_ball)
+        {
+            valid_point = true;
+        }
+        
+    }
+
+    
+}
---
+++ Assets/2_Scripts/Score/ScoreGM.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreGM : MonoBehaviour {
+
+    // Paniers 
+    public Panier panier_blue;
+    public Panier panier_red;
+
+    // Scores
+    public int score_blue;
+    public int score_red;
+
+
+    private void checkPoint(ref int score, Panier panier)
+    {
+       if (panier.valid_point)
+        {
+            score++;
+            panier.valid_point = false;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkPoint(ref score_blue, panier_red);
+        checkPoint(ref score_red, panier_blue);
+	}
+}
