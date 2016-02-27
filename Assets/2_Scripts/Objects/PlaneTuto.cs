671e0cac7e96682507b6dd17cabf452375c80a68
ShaigroRB <1997robin@laposte.net>
2016-02-27 22:28:44 +0100

    wall added (tuto again)

---
+++ Assets/2_Scripts/Objects/PlaneTuto.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneTuto : MonoBehaviour
+{
+    bool toggleGUI;
+    public string phrase;
+    Rect labelRect;
+
+
+    void OnTriggerEnter(Collider player) 
+    {
+        toggleGUI = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        toggleGUI = false;
+    }
+
+    void OnGUI()
+    {
+        if (toggleGUI)
+        {
+            labelRect = GUILayoutUtility.GetRect(new GUIContent(phrase), "label");
+            GUI.contentColor = Color.black;
+            GUIStyle guiStyle = new GUIStyle();
+            guiStyle.fontSize = 40;
+            GUI.Label(labelRect, phrase, guiStyle);
+        }
+    }
+}
