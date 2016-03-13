d6ddd6b119a49145cc5af22a1b4a7ca5e5f87aa1
FranckyVincent <antoine.sainson@gmail.com>
2016-03-13 11:54:32 +0100

    Finally...    Basesv0.2 + Login final

---
+++ Assets/2_Scripts/Bases/Robot_tuto.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Robot_tuto : MonoBehaviour {
+
+    public Object game;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision col) {
+
+        Destroy(game);
+	}
+}
