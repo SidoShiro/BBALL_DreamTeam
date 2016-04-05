7456e8b56cbc1b708234e26ee6be12d2ea1ac9dd
Slamjack <murlouk@gmail.com>
2016-04-05 20:15:38 +0200

    Komments

--- Assets/2_Scripts/_Interface/m_Custom.cs
+++ Assets/2_Scripts/_Interface/m_Custom.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System;
 
-
+/// <summary>
+/// Enum used for team affectation
+/// </summary>
 public enum Team
 {
     SPE = 0,
@@ -9,37 +11,37 @@ public enum Team
     RED = 2
 }
 
+/// <summary>
+/// Enum used for damage affectation
+/// </summary>
 public enum DamageType
 {
-    Hazard,
-    Explosion
+    Hazard = 0,
+    Explosion = 1
 }
 
+/// <summary>
+/// This static class is used to define global types and methods in the project
+/// Its uses are but not limited to :
+/// Collision layering
+/// Render layering
+/// Custom enumerations (see above)
+/// ...
+/// </summary>
 public class m_Custom : MonoBehaviour
 {
-    private static LayerMask _layerMaskBLU = LayerMask.GetMask("Default", "RED");
-    private static LayerMask _layerMaskRED = LayerMask.GetMask("Default", "BLU");
+    private static LayerMask _layerMaskBLU = LayerMask.GetMask("Default", "RED");               //Collision layer of BLU team
+    private static LayerMask _layerMaskRED = LayerMask.GetMask("Default", "BLU");               //Collision layer of RED team
+    private static LayerMask _layerMaskRocket = LayerMask.GetMask("Default", "BLU", "RED");     //Collision layer of rockets
 
-    private static LayerMask _layerMaskNOSPE = ~LayerMask.GetMask("NORENDER", "SPE");
-    private static LayerMask _layerMaskWTSPE = ~LayerMask.GetMask("NORENDER");
-
-    private static LayerMask _layerMaskRocket = LayerMask.GetMask("Default", "BLU", "RED");
-
-    public static Color GetColorFromTeam(int team)
-    {
-        switch (team)
-        {
-            case 1:
-                return new Color32(52, 152, 219, 255);
-
-            case 2:
-                return new Color32(231, 76, 60, 255);
-
-            default:
-                return new Color32(155, 89, 182, 255);
-        }
-    }
-
+    private static LayerMask _layerMaskNOSPE = ~LayerMask.GetMask("NORENDER", "SPE");           //Rendering layer of RED team
+    private static LayerMask _layerMaskWTSPE = ~LayerMask.GetMask("NORENDER");                  //Rendering layer of RED team
+    
+    /// <summary>
+    /// Used to get the team color according to team (Currently hardcoded)
+    /// </summary>
+    /// <param name="team">Team to get the color from</param>
+    /// <returns>Color of the team</returns>
     public static Color GetColorFromTeam(Team team)
     {
         switch (team)
@@ -55,6 +57,11 @@ public class m_Custom : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Get collision layer according to team
+    /// </summary>
+    /// <param name="team">Team to get the layer from</param>
+    /// <returns>Collision layer of team</returns>
     public static LayerMask GetLayerFromTeam(Team team)
     {
         switch (team)
@@ -71,6 +78,11 @@ public class m_Custom : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Used to generate random player names
+    /// </summary>
+    /// <param name="maxSize">Size of the guid</param>
+    /// <returns>Random string of size maxSize</returns>
     public static string RandomGUID(int maxSize)
     {
         return Guid.NewGuid().ToString().Substring(0, maxSize);
