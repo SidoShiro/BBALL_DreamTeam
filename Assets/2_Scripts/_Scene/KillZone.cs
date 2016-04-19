e265be25f6f8f75daf23ddb5cd111dd2c8b9ac94
Slamjack <murlouk@gmail.com>
2016-04-19 11:59:57 +0200

    Added KillZone

---
+++ Assets/2_Scripts/_Scene/KillZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// This script, when attached to any object with a collider, will kill any player colliding with it
+/// </summary>
+public class KillZone : MonoBehaviour
+{
+    /// <summary>
+    /// Called when something collide
+    /// </summary>
+    /// <param name="col">Collision information</param>
+    void OnCollisionEnter(Collision col)
+    {
+        GameObject playerRigidBody = col.gameObject;    //Get object related to collision 
+        if (playerRigidBody.tag == "Player")            //If it's a player, kills it
+        {
+            playerRigidBody.GetComponent<PlayerCall>().Call_KillPlayer("");
+        }
+    }
+}
