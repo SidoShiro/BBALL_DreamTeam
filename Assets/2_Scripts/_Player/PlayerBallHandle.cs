dde3fedd459cf029788a241bb35d11cafdf55fd7
Slamjack <murlouk@gmail.com>
2016-04-19 17:02:57 +0200

    Added Trail+Color

--- Assets/2_Scripts/_Player/PlayerBallHandle.cs
+++ Assets/2_Scripts/_Player/PlayerBallHandle.cs
@@ -10,9 +10,37 @@ public class PlayerBallHandle : NetworkBehaviour
     //TODO : Comment
     [Header("References(Player)")]
     [SerializeField]
+    private PlayerStats playerStats;
+    [SerializeField]
     private GameObject playerBallModel;
+    [SerializeField]
+    private TrailRenderer balltrailRenderer;
+
+    [Header("Parameters")]
+    [SerializeField]
+    private Material BLUTrailMaterial;
+    [SerializeField]
+    private Material REDTrailMaterial;
 
     public bool isCarrying; //Ball carrying toggle
+
+    void Start()
+    {
+        switch (playerStats.playerTeam)
+        {
+            case Team.BLU:
+                balltrailRenderer.material = BLUTrailMaterial;
+                break;
+
+            case Team.RED:
+                balltrailRenderer.material = REDTrailMaterial;
+                break;
+
+            default:
+                balltrailRenderer.material = BLUTrailMaterial;
+                break;
+        }
+    }
 
     [ClientRpc]
     public void Rpc_PickBall()
