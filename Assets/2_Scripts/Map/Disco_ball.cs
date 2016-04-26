b6108a36be6e003aa5554247ce3e2b494d41c2d3
FranckyVincent <antoine.sainson@gmail.com>
2016-04-26 17:01:01 +0200

    bball_disco v.1.0+ ball_gap changes
    La boule disco fonctionne et elle est edgy, bball_gap est en cours de
    construction.

---
+++ Assets/2_Scripts/Map/Disco_ball.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disco_ball : MonoBehaviour {
+
+    public int Rotate_speed;
+
+	// Update is called once per frame
+	void Update () {
+       // transform.Rotate(Vector3.right * Time.deltaTime * Rotate_speed);
+        transform.Rotate(Vector3.up * Time.deltaTime * Rotate_speed);
+        transform.Rotate(Vector3.left * Time.deltaTime * Rotate_speed / 2);
+    }
+}
