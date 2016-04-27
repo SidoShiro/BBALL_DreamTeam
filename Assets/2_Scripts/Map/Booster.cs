faec308aca4315fc2cbc8744ba1e74f161df3506
FranckyVincent <antoine.sainson@gmail.com>
2016-04-27 15:18:34 +0200

    bball_standard changes + script tramplin + script booster

---
+++ Assets/2_Scripts/Map/Booster.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Booster : MonoBehaviour {
+
+    public int force;
+	
+	// Update is called once per frame
+	void OnTriggerStay (GameObject player) {
+
+        player.GetComponent<Rigidbody>().velocity *= 2;
+	
+	}
+}
---
+++ Assets/2_Scripts/Map/Tremplin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tremplin : MonoBehaviour {
+
+    public int force;
+
+	
+	// Update is called once per frame
+	void OnTriggerEnter (GameObject player) {
+        player.GetComponent<Rigidbody>().AddForce(transform.up * force);
+	}
+}
