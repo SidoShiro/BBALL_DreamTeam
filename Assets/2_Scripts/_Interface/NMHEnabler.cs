79a71eda642c8f56cf771e17d0a2f82c4ef1db90
Slamjack <murlouk@gmail.com>
2016-04-29 11:12:37 +0200

    Hidden NMH

---
+++ Assets/2_Scripts/_Interface/NMHEnabler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NMHEnabler : MonoBehaviour
+{
+    public CustomNMHUD customNMH;
+
+    void Start()
+    {
+        customNMH.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F4))
+        {
+            customNMH.enabled = !customNMH.enabled;
+        }
+    }
+}
