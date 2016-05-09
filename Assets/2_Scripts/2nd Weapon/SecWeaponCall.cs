b3f90a0cd58691b23871c9336c271b58db58002b
Hastur <maxence.caronlasne@gmail.com>
2016-05-09 17:02:36 +0200

    Created bases for the Secondary Weapon system
    - SecWeapon_Test : Test secondary weapon
    - SecWeaponCall : function caller, will call all the functions about
    secondary weapons

---
+++ Assets/2_Scripts/2nd Weapon/SecWeaponCall.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+
+/// <summary>
+/// Ce script gere tous les calls destines aux armes secondaires. 
+/// </summary>
+public class SecWeaponCall : NetworkBehaviour {
+
+	public enum SecWeapon { None, secTest, secSnipar, secGrenadeLauncher};
+
+
+    [Header("References : Secondary Weapons")]
+    #region References : Secondary Weapons
+    [SerializeField]
+    private SecWeapon_Test secWeapon_Test;
+    #endregion
+
+    /// <summary>
+    /// Arme secondaire actuelle.
+    /// </summary>
+    [SyncVar]
+	private SecWeapon currentSecWeapon;
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentSecWeapon = SecWeapon.None;
+	}
+	
+
+    #region Fonctions privees
+
+    private void KillCurrentSecWeapon()
+    {
+        if (currentSecWeapon == SecWeapon.secTest)
+        {
+            secWeapon_Test.OnKill();
+        }
+    }
+
+    private void SpawnNewSecWeapon()
+    {
+        if (currentSecWeapon == SecWeapon.secTest)
+        {
+            secWeapon_Test.OnSpawn();
+        }
+    }
+
+    #endregion
+
+    #region Calls
+    /// <summary>
+    /// Permet de remplacer l'arme secondaire actuelle par une autre.
+    /// </summary>
+    /// <param name="sec_weapon_type"></param>
+    public void SecCall_SwitchCurrentWeapon(SecWeapon sec_weapon_type) {
+
+		if (sec_weapon_type != currentSecWeapon)
+        {
+            KillCurrentSecWeapon();
+            currentSecWeapon = sec_weapon_type;
+            SpawnNewSecWeapon();
+		}
+	}
+
+    /// <summary>
+    /// Appelle un coup de feu de l'arme secondaire actuelle.
+    /// </summary>
+    public void SecCall_Fire()
+    {
+        if (currentSecWeapon == SecWeapon.secTest)
+        {
+            secWeapon_Test.CallFire();
+        }
+    }
+    
+    /// <summary>
+    /// Appelle un rechargement de l'arme secondaire actuelle.
+    /// </summary>
+    public void SecCall_Reload()
+    {
+        if (currentSecWeapon == SecWeapon.secTest)
+        {
+            secWeapon_Test.CallReload();
+        }
+    }
+    #endregion
+
+
+}
---
+++ Assets/2_Scripts/2nd Weapon/SecWeapon_Test.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Arme secondaire de test.
+/// </summary>
+public class SecWeapon_Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnSpawn()
+    {
+
+    }
+
+    public void OnKill()
+    {
+
+    }
+
+    public void CallFire()
+    {
+
+    }
+
+    public void CallReload()
+    {
+
+    }
+}
