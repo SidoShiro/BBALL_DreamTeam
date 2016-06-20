24b9e8378192408451c28e167d0379ca52fc2825
Slamjack <murlouk@gmail.com>
2016-06-20 10:49:14 +0200

    Connection via IP now in Menu and working
    Still may need testing

--- Assets/2_Scripts/_Interface/NetworkOverlord.cs
+++ Assets/2_Scripts/_Interface/NetworkOverlord.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class NetworkOverlord : NetworkManager
@@ -6,5 +7,21 @@ public class NetworkOverlord : NetworkManager
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
         base.OnServerAddPlayer(conn, playerControllerId);
+    }
+
+    public void ConnectToGame()
+    {
+        Text AdressText = GameObject.Find("AdressText").GetComponent<Text>();
+        if(AdressText != null)
+        {
+            Debug.Log("Trying to connect to : " + AdressText.text);
+            networkAddress = AdressText.text;
+            StartClient();
+        }
+        else
+        {
+            Debug.Log("Error in finding adress for Conection");
+        }
+        
     }
 }
